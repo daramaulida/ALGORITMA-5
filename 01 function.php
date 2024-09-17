807740ebf7c0c92869dfe888379bf114a6b3604c
daramaulida <daraksp291@gmail.com>
2024-09-17 13:25:20 +0700

    function

---
+++ 01 function.php
@@ -0,0 +1,15 @@
+<?php
+// mmbuat fungsi
+function perkenalan(){
+  echo "Assalamulaikum, ";
+  echo "Perkenalkan, nama saya Dara<br/>";
+  echo "Senang berkenalan dengan anda<br/>";
+}
+// memanggil fungsi yang sudah dibuat
+perkenalan();
+
+echo "<hr>";
+// memanggilnya lagi
+perkenalan();
+echo "<p><strong>By:Dara Maulida";
+?>
