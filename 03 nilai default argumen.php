69c1cd4a1cdca5368d6c56b654a63325b193f872
daramaulida <daraksp291@gmail.com>
2024-09-17 13:45:17 +0700

    nilai default argumen

---
+++ 03 nilai default argumen.php
@@ -0,0 +1,14 @@
+<?php
+
+function nama_bulan($bulan, $tahun = 2016) {
+	echo $bulan . ' ' . $tahun;
+}
+nama_bulan('Januari'); // Hasil Januari 2016
+echo "<hr>";
+nama_bulan('Februari');// Hasil Februari 2016
+echo "<hr>";
+nama_bulan('Maret');// Hasil Maret 2016
+echo "<hr>";
+nama_bulan('April');// Hasil April 2016
+echo "<hr>";
+echo "<p><strong>By:Dara Maulida";
