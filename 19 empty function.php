902d51e5c4cf560d93af881b97685a9b75f2a52f
daramaulida <daraksp291@gmail.com>
2024-09-19 10:55:40 +0700

    empty function

---
+++ 19 empty function.php
@@ -0,0 +1,25 @@
+<?php
+
+//Penggunaan Fungsi empty
+$var1 = "";
+$var2 = 0;
+$var3 = false;
+$var4 = array();
+
+if(empty($var1)) {
+    echo '$var1 kosong';
+}
+echo "<br>";
+if(empty($var2)) {
+    echo '$var2 kosong';
+}
+echo "<br>";
+if(empty($var3)) {
+    echo '$var3 kosong';
+}
+echo "<br>";
+if(empty($var4)) {
+    echo '$var4 kosong';
+}
+
+echo "<p><strong>By:Dara Maulida";
