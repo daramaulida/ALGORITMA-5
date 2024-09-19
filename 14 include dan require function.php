29eb8e05e96a4b6144cd0f5b3323a418e3f10c06
daramaulida <daraksp291@gmail.com>
2024-09-19 09:27:13 +0700

    include & require function

---
+++ 14 include dan require function.php
@@ -0,0 +1,14 @@
+<?php
+
+//include
+include '13 return function.php';
+
+//require
+echo "<hr>";
+require '06 fungsi dengan parameter.php';
+
+//include_once dan require_once
+echo "<hr>";
+include_once '03 nilai default argumen.php';
+echo "<hr>";
+require_once '01 function.php';
