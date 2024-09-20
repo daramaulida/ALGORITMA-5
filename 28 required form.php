06e8a8f053d7c87dd3bc2b25cd2cf80ce23f8e28
daramaulida <daraksp291@gmail.com>
2024-09-20 09:42:16 +0700

    required form

---
+++ 28 required form.php
@@ -0,0 +1,10 @@
+<form method="post" action="28 proses_form.php">
+  Nama: <input type="text" name="nama" required>
+  <br>
+  Email: <input type="email" name="email" required>
+  <br>
+  <input type="submit" value="Kirim">
+  <br>
+  <p><strong>By:Dara Maulida
+
+</form>
