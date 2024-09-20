fbf8d258244f7c5ca8673955a5204513d8bfbe21
daramaulida <daraksp291@gmail.com>
2024-09-20 09:51:43 +0700

    form url

---
+++ 29 form url.php
@@ -0,0 +1,4 @@
+<form action="29 search.php" method="get">
+  Cari: <input type="text" name="query">
+  <input type="submit" value="Search">
+</form>
---
+++ 29 search.php
@@ -0,0 +1,7 @@
+<?php
+
+if(isset($_GET['query'])) {
+    $searchTerm = $_GET['query'];
+    // Proses searchTerm untuk menampilkan hasil pencarian
+  }
+  
